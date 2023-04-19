630a040f9b6ae129e3dad5e547c8160c1fa1f142
Thiện Tạ Khánh <takhanhthient3@gmail.com>
2023-04-19 14:38:15 +0700

    uppp

---
+++ pages/[slug].tsx
@@ -0,0 +1,41 @@
+// @ts-ignore
+import fetchMeta from "fetch-meta-tags";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { domain } from "../domain";
+
+export async function getStaticPaths() {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps({ params }: { params: any }) {
+  const data = await fetchMeta(domain + "/" + params.slug);
+  return {
+    props: data,
+  };
+}
+
+export default function App({ url, title, description, icon, image }: any) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace(url);
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+        <meta name="og:image" content={image} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href={icon} />
+      </Head>
+    </>
+  );
+}
